28878af168d357070194ac40ad1ed50105158377
slakj <138445828+Edwigge@users.noreply.github.com>
2025-06-11 00:58:14 +0200

    Add files via upload

--- Šifre.py
+++ Šifre.py
@@ -12,12 +12,17 @@ rIII = ['J', 'V', 'I', 'U', 'B', 'H', 'T', 'C', 'D', 'Y', 'A', 'K', 'E', 'Q', 'Z
 
 ukw = ['q', 'y', 'h', 'o', 'g', 'n', 'e', 'c', 'v', 'p', 'u', 'z', 't', 'f', 'd', 'j', 'a', 'x', 'w', 'm', 'k', 'i', 's', 'r', 'b', 'l']
 UKW = ['Q', 'Y', 'H', 'O', 'G', 'N', 'E', 'C', 'V', 'P', 'U', 'Z', 'T', 'F', 'D', 'J', 'A', 'X', 'W', 'M', 'K', 'I', 'S', 'R', 'B', 'L']
-#Seznam malih in velikih črk v kolutu za obračanje v enigmi.
+#Seznam malih in velikih črk v reflektorju v enigmi.
 
 import time
+import random
 
-#--------------------------
+
+
+#----------------------
 #Funkcije za vse šifre.
+
+
 
 def cezar(geslo):
     #Funkcija izvede cezarjevo šifro z zamikom 3 črke.
@@ -108,67 +113,133 @@ def autokey(geslo, ključ):
             c += 1
     print(rez)
 
+
+
+
 def enigma(geslo, r1, r2, r3):
-    #Šifrirna naprava enigma. 
-    začrka = ""
+    #Osnovna verzija Enigme, brez plugboarda.
     rez = ""
     r1 = int(r1) % 26
     r2 = int(r2) % 26
     r3 = int(r3) % 26
-    geslo = str(geslo)
+
     for i in range(len(geslo)):
-        if geslo[i] in abc:
-            #Črko 'premakne' skozi vse rotorje in nazaj.
-            začrka = ri[abc.index(geslo[i]) - r1]
-            začrka = rii[abc.index(geslo[i]) - r2]
-            začrka = riii[abc.index(geslo[i]) - r3]
-            začrka = ukw[abc.index(geslo[i])]
-            začrka = riii[abc.index(geslo[i]) - r3]
-            začrka = rii[abc.index(geslo[i]) - r2]
-            začrka = ri[abc.index(geslo[i]) - r1]
-            rez += začrka
-            if r1 < 25:
-                #Zamakne rotorje za eno pozicijo.
-                r1 +=1
-            else:
-                r1 = 0
-                if r2 < 25:
-                    r2 += 1
-                else:
-                    r2 = 0
-                    if r3 < 25:
-                        r3 +=1
-                    else:
-                        r3 = 0
-        elif geslo[i] in ABC:
-            začrka = rI[ABC.index(geslo[i]) - r1]
-            začrka = rII[ABC.index(geslo[i]) - r2]
-            začrka = rIII[ABC.index(geslo[i]) - r3]
-            začrka = UKW[ABC.index(geslo[i])]
-            začrka = rIII[ABC.index(geslo[i]) - r3]
-            začrka = rII[ABC.index(geslo[i]) - r2]
-            začrka = rI[ABC.index(geslo[i]) - r1]
-            rez += začrka
-            if r1 < 25:
-                r1 +=1
-            else:
-                r1 = 0
-                if r2 < 25:
-                    r2 += 1
-                else:
-                    r2 = 0
-                    if r3 < 25:
-                        r3 +=1
-                    else:
-                        r3 = 0
+        znak = geslo[i]
+
+        if znak in abc:
+            idx = (abc.index(znak) + r1) % 26
+            znak = ri[idx]
+
+            idx = (abc.index(znak) + r2) % 26
+            znak = rii[idx]
+
+            idx = (abc.index(znak) + r3) % 26
+            znak = riii[idx]
+
+            # Reflector
+            idx = abc.index(znak)
+            znak = ukw[idx]
+
+            # Reverse path
+            idx = riii.index(znak)
+            znak = abc[(idx - r3) % 26]
+
+            idx = rii.index(znak)
+            znak = abc[(idx - r2) % 26]
+
+            idx = ri.index(znak)
+            znak = abc[(idx - r1) % 26]
+
+            rez += znak
+
+        elif znak in ABC:
+            idx = (ABC.index(znak) + r1) % 26
+            znak = rI[idx]
+
+            idx = (ABC.index(znak) + r2) % 26
+            znak = rII[idx]
+
+            idx = (ABC.index(znak) + r3) % 26
+            znak = rIII[idx]
+
+            # Reflector
+            idx = ABC.index(znak)
+            znak = UKW[idx]
+
+            # Reverse path
+            idx = rIII.index(znak)
+            znak = ABC[(idx - r3) % 26]
+
+            idx = rII.index(znak)
+            znak = ABC[(idx - r2) % 26]
+
+            idx = rI.index(znak)
+            znak = ABC[(idx - r1) % 26]
+
+            rez += znak
+
         else:
-            rez += geslo[i]
+            rez += znak
+
+        # Posodobi rotorje
+        r1 = (r1 + 1) % 26
+        if r1 == 0:
+            r2 = (r2 + 1) % 26
+            if r2 == 0:
+                r3 = (r3 + 1) % 26
+
     print(rez)
 
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
+#Evklidov algoritem  
+def modinv(e, phi):
+    d_old, d_new = 0, 1
+    r_old, r_new = phi, e
+    while r_new != 0:
+        quotient = r_old // r_new
+        d_old, d_new = d_new, d_old - quotient * d_new
+        r_old, r_new = r_new, r_old - quotient * r_new
+    return d_old % phi
 
-#--------------------------
+def praštevilo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def gen_praštevili():
+    while True:
+        p = random.randint(100, 300)
+        q = random.randint(100, 300)
+        if praštevilo(p) and praštevilo(q) and p != q:
+            return p, q
+
+def gen_ključa():
+    p, q = gen_praštevili()
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 65537  # pogosto uporabljen javni eksponent
+    while gcd(e, phi) != 1:
+        e = random.randrange(2, phi)
+    d = modinv(e, phi)
+    return ((e, n), (d, n))
+
+def rsa(msg, javni):
+    e, n = javni
+    msg_nums = [ord(char) for char in msg]
+    šifra = [pow(m, e, n) for m in msg_nums]
+    print(šifra)
+
+#------------------------------------
 #Funkcije za dešifriranje vseh šifer.
+
+
 
 def vigenere_res(geslo, ključ):
     #Reševanje šifre vigenere. 
@@ -215,15 +286,22 @@ def autokey_res(geslo, ključ):
             c += 1
     print(rez)
 
+def rsa_res(šifra, privat):
+    d, n = privat
+    decrypted_nums = [pow(c, d, n) for c in šifra]
+    print(''.join(chr(m) for m in decrypted_nums))
 
 
-#--------------------------
+#----------------------------------------------------------------
 #Funkciji za kodiranje in dekodiranje (interakcija z uporabnokom)
-    
+
+
+
+
 def kodiranje():
     geslo = input('Kaj želiš zakodirati? ')
     if geslo == "":
-        print('-------------------- \nVpišite geslo, ki bi ga radi zakodirali. \nČe želite končati, vpišite \'STOP\' \n--------------------')
+        print('-------------------- \nVpišite besedilo, ki bi ga radi zakodirali. \nČe želite končati, vpišite \'STOP\' \n--------------------')
         time.sleep(0.2)
         kodiranje()
     if geslo != 'STOP':
@@ -235,7 +313,7 @@ def kodiranje():
             if črka_g == 'ć':
                 črka_g = 'c'
             if črka_g == 'đ':
-                črka_g = 'd'
+                črka_g = 'dz'
             if črka_g == 'š':
                 črka_g = 's'
             if črka_g == 'ž':
@@ -245,13 +323,13 @@ def kodiranje():
             if črka_g == 'Ć':
                 črka_g = 'C'
             if črka_g == 'Đ':
-                črka_g = 'D'
+                črka_g = 'DZ'
             if črka_g == 'Š':
                 črka_g = 'S'
             if črka_g == 'Ž':
                 črka_g = 'Z'
             geslo_1 += črka_g
-        odg = input('Katero šifro želiš? ')
+        odg = input('Katero šifro želiš? ').lower()
         if odg == 'c':
             cezar(geslo_1)
         elif odg == 'cz':
@@ -262,15 +340,15 @@ def kodiranje():
             ključ_1 = ""
             for i  in range(len(ključ)):
                 črka_k = ključ[i]
-                if črka_k == 'č' or črka_k == 'Č':
+                if črka_k.lower() == 'č':
                     črka_k = 'c'
-                if črka_k == 'č' or črka_k == 'Ć':
+                if črka_k.lower() == 'ć':
                     črka_k = 'c'
-                if črka_k == 'đ' or črka_k == 'Đ':
-                    črka_k = 'd'
-                if črka_k == 'š' or črka_k == 'Š':
+                if črka_k.lower() == 'đ':
+                    črka_k = 'dz'
+                if črka_k.lower() == 'š':
                     črka_k = 's'
-                if črka_k == 'ž' or črka_k == 'Ž':
+                if črka_k.lower() == 'ž':
                     črka_k = 'z'
                 črka_k = črka_k.lower()
                 ključ_1 += črka_k
@@ -293,20 +371,33 @@ def kodiranje():
                 kodiranje()
             if r1.isdigit() != False and r2.isdigit() != False and r3.isdigit() != False:
                 enigma(geslo_1, r1, r2, r3)
+        elif odg in ['r', 'rsa']:
+            imam = input('Že imate javni ključ? ').lower()
+            if imam in ['j', 'd']:
+                javni = eval(input('Vpišite javni ključ: '))
+                rsa(geslo_1, javni)
+            elif imam == 'n':
+                javni, privat = gen_ključa()
+                print('Vaš javni ključ je \'' + str(javni) + '\' in vaš zasebni ključ je \'' + str(privat) + '\'.')
+                rsa(geslo_1, javni)
+            else:
+                print('-------------------- \nProsim, vnesite veljaven odgovor. \nd - da, imam svoj javni ključ \nn - ne, želim nov javni in privatni ključ \n--------------------')
         elif odg != 'STOP':
-            print('-------------------- \nZa odgovor vpišite eno od naslednjih črk: \nc - cezarjeva šifra \ncz - cezarjeva šifra s poljubnim zamikom \nv - šifra vigenere \na - šifra autokey \ne - enigma \nČe želite končati, vpišite \'STOP\'. \n--------------------')
+            print('-------------------- \nZa odgovor vpišite eno od naslednjih črk: \nc - cezarjeva šifra \ncz - cezarjeva šifra s poljubnim zamikom \nv - šifra vigenere \na - šifra autokey \ne - Enigma \nr - RSA \nČe želite končati, vpišite \'STOP\'. \n--------------------')
             time.sleep(0.2)
             kodiranje()
         kdaj = False
         while kdaj == False:
             še = input('Še eno? ')
-            if še == 'd' or še == 'D' or še == 'j' or še == 'J':
+            if še.lower() in ['d', 'j']:
+                print('\n')
                 kodiranje()
                 kdaj = True
-            elif še == 'dek' or še == 'Dek' or še == 'DEK':
+            elif še.lower() == 'dek':
+                print('\n')
                 dekodiranje()
                 kdaj = True
-            elif še == 'n' or še == 'N':
+            elif še.lower() == 'n':
                 kdaj = True
                 return
             else:
@@ -316,11 +407,11 @@ def kodiranje():
 def dekodiranje():
     geslo = input('Kaj želiš dekodirati? ')
     if geslo == "":
-        print('-------------------- \nVpišite geslo, ki bi ga radi dekodirali. \nČe želite končati, vpišite \'STOP\' \n--------------------')
+        print('-------------------- \nVpišite besedilo, ki bi ga radi dekodirali. \nČe želite končati, vpišite \'STOP\' \n--------------------')
         time.sleep(0.2)
         dekodiranje()
     if geslo != 'STOP':
-        odg = input('S katero šifro je šifra zakodirana? ')
+        odg = input('S katero šifro je besedilo zakodirano? ').lower()
         if odg == 'c':
             cezar_zamik(geslo, '23')
         elif odg == 'cz':
@@ -336,7 +427,7 @@ def dekodiranje():
                 if črka_k == 'č' or črka_k == 'Ć':
                     črka_k = 'c'
                 if črka_k == 'đ' or črka_k == 'Đ':
-                    črka_k = 'd'
+                    črka_k = 'dz'
                 if črka_k == 'š' or črka_k == 'Š':
                     črka_k = 's'
                 if črka_k == 'ž' or črka_k == 'Ž':
@@ -348,21 +439,39 @@ def dekodiranje():
             else:
                 autokey_res(geslo, ključ)
         elif odg == 'e':
-            print('Žal dešifriranje enigme trenutno še ni mogoče. Prototip dešifrerja je pospravljen nekje na dnu kode. Hvala za razumevanje! :)')
+            r1 = input('Pozicija rotorja 1? ')
+            if r1.isdigit() == False:
+                print('-------------------- \nZa rotorje lahko vpišite le naravna števila. \n--------------------')
+                kodiranje()
+            r2 = input('Pozicija rotorja 2? ')
+            if r2.isdigit() == False:
+                print('-------------------- \nZa rotorje lahko vpišite le naravna števila. \n--------------------')
+                kodiranje()
+            r3 = input('Pozicija rotorja 3? ')
+            if r3.isdigit() == False:
+                print('-------------------- \nZa rotorje lahko vpišite le naravna števila. \n--------------------')
+                kodiranje()
+            if r1.isdigit() != False and r2.isdigit() != False and r3.isdigit() != False:
+                enigma(geslo, r1, r2, r3)
+        elif odg in ['r', 'rsa']:
+            privat = eval(input('Kakšen je vaš zasebni ključ? '))
+            rsa_res(eval(geslo), privat)
         elif odg != 'STOP':
-            print('-------------------- \nZa odgovor vpišite eno od naslednjih črk: \nc - cezarjeva šifra \ncz - cezarjeva šifra s poljubnim zamikom \nv - šifra vigenere \na - šifra autokey \ne - enigma \nČe želite končati, vpišite \'STOP\'. \n--------------------')
+            print('-------------------- \nZa odgovor vpišite eno od naslednjih črk: \nc - cezarjeva šifra \ncz - cezarjeva šifra s poljubnim zamikom \nv - šifra vigenere \na - šifra autokey \ne - Enigma \nr - RSA \nČe želite končati, vpišite \'STOP\'. \n--------------------')
             time.sleep(0.2)
             dekodiranje()
         kdaj = False
         while kdaj == False:
             še = input('Še eno? ')
-            if še == 'd' or še == 'D' or še == 'j' or še == 'J':
+            if še.lower() in ['d', 'j']:
+                print('\n')
                 dekodiranje()
                 kdaj = True
-            elif še == 'kod' or še == 'Kod' or še == 'KOD':
+            elif še.lower() in ['k', 'kod', 'z', 'zak']:
+                print('\n')
                 kodiranje()
                 kdaj = True
-            elif še == 'n' or še == 'N':
+            elif še.lower() == 'n':
                 kdaj = True
                 return
             else:
@@ -372,199 +481,14 @@ def dekodiranje():
 def kvizek():
     #Vprašanje, ki ga program postavi čisto na začetku
     kviz = input('Ali želite nekaj zakodirati ali dekodirati? ')
-    if kviz == 'kod' or kviz == 'Kod' or kviz == 'KOD' or kviz == 'zak' or kviz == 'Zak' or kviz == 'ZAK':
+    if kviz.lower() in ['k', 'z', 'kod', 'zak']:
         kodiranje()
-    elif kviz == 'dek' or kviz == 'Dek' or kviz == 'DEK':
+    elif kviz.lower() in ['d', 'dek']:
         dekodiranje()
     elif kviz != 'STOP':
         print('-------------------- \nProsim, vnesite veljaven odgovor. \nkod - kodiranje \ndek - dekodiranje \nČe ste si premislili in ne želite narediti nič, vpišite \'STOP\' \n--------------------')
         kvizek()
 
+
+print('Legenda: \nk - kodiranje \nd - dekodiranje \n-------------------- \nc - cezarjeva šifra \ncz - cezarjeva šifra s poljubnim zamikom \nv - šifra vigenere \na - šifra autokey \ne - Enigma \nr - RSA\n')
 kvizek()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#-------------------------------------
-#Prototip kode za dešifriranje enigme (ne vem zakaj ne dela)
-
-'''
-def enigma_res(geslo, r1, r2, r3):
-    #Dešifriranje enigme.
-    začrka = ""
-    rez = ""
-    poz = 0
-    r1 %= 26
-    r2 %= 26
-    r3 %= 26
-    geslo = str(geslo)
-    for i in range(len(geslo)):
-        if geslo[i] in abc:
-            #Črko 'premakne' skozi vse rotorje in nazaj.
-            začrka = ri[abc.index(geslo[i]) - r1]
-            začrka = rii[abc.index(geslo[i]) - r2]
-            začrka = riii[abc.index(geslo[i]) - r3]
-            začrka = ukw[abc.index(geslo[i])]
-            začrka = riii[abc.index(geslo[i]) - r3]
-            začrka = rii[abc.index(geslo[i]) - r2]
-            začrka = ri[abc.index(geslo[i]) - r1]
-            rez += začrka
-            if r1 < 25:
-                r1 +=1
-            else:
-                r1 = 0
-                if r2 < 25:
-                    r2 += 1
-                else:
-                    r2 = 0
-                    if r3 < 25:
-                        r3 +=1
-                    else:
-                        r3 = 0
-        elif geslo[i] in ABC:
-            začrka = rI[ABC.index(geslo[i]) - r1]
-            začrka = rII[ABC.index(geslo[i]) - r2]
-            začrka = rIII[ABC.index(geslo[i]) - r3]
-            začrka = UKW[ABC.index(geslo[i])]
-            začrka = rIII[ABC.index(geslo[i]) - r3]
-            začrka = rII[ABC.index(geslo[i]) - r2]
-            začrka = rI[ABC.index(geslo[i]) - r1]
-            rez += začrka
-            if r1 < 25:
-                r1 +=1
-            else:
-                r1 = 0
-                if r2 < 25:
-                    r2 += 1
-                else:
-                    r2 = 0
-                    if r3 < 25:
-                        r3 +=1
-                    else:
-                        r3 = 0
-        else:
-            rez += geslo[i]
-    print(rez)
-'''
-
-
-#-----------------------------
-#Dodatek k prototipu
-
-'''
-for e in range(len(geslo)):
-        if geslo[e] in abc or geslo[e] in ABC:
-            poz += 1
-    for j in range(poz):
-        if r1 < 25:
-            #Zamakne vse rotorje na svojo končno pozicijo.
-            r1 +=1
-        else:
-            r1 = 0
-            if r2 < 25:
-                r2 += 1
-            else:
-                r2 = 0
-                if r3 < 25:
-                    r3 +=1
-                else:
-                    r3 = 0
-'''
-
-
